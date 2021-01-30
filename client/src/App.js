a3d5ce2280a82a8de09e534f6e69d25f6677443b
jpandrade <jpandrade@uninorte.edu.co>
2021-01-29 19:30:53 -0500

    Set up fetching posts - client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux'
 
+import { getPosts } from './actions/posts'; 
 import Posts from './components/posts/Posts';
 import Form from './components/form/Form';
 import success from './images/success.png';
@@ -8,6 +10,12 @@ import useStyles from './Styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return(
         <Container maxWidth = "lg">
             <AppBar className = {classes.appBar} position = "static" color = "inherit">
