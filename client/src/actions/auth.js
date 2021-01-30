dab764cde915034407d1f974ef221ce6f3273db4
jpandrade <jpandrade@uninorte.edu.co>
2021-01-30 07:53:56 -0500

    Sign in completed with jwt

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,6 +3,9 @@ import * as api from '../api/index.js';
 
 export const signin = (formData, router) => async (dispatch) => {
   try {
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
     
     router.push('/');
   } catch (error) {
@@ -12,7 +15,10 @@ export const signin = (formData, router) => async (dispatch) => {
 
 export const signup = (formData, router) => async (dispatch) => {
   try {
-    
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+
     router.push('/');
   } catch (error) {
     console.log(error);
