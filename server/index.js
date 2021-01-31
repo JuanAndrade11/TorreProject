dbd6342d2ef9ecc7f17b3a9afdf999b48afecfad
jpandrade <jpandrade@uninorte.edu.co>
2021-01-31 17:37:36 -0500

    ss

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/',(req,res)=>{
     res.send("Welcome to Torre:ss API")
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
