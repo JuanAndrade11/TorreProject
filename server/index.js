fd2f4aefc6a00bfe57c02ce3f625ad9c4a3a4957
jpandrade <jpandrade@uninorte.edu.co>
2021-01-31 20:20:21 -0500

    DEPLOYMENT

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.get('/',(req,res)=>{
 const CONNECTION_URL = 'mongodb://admin:admin@torress-mongo-production/torress'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
